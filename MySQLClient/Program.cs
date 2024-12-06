7d59b430b5f8689bc12c6f5ebbff8f54b6c4fc38
БайбатыровМ <@206-P>
2024-12-06 21:01:36 +0500

    Добавьте файлы проекта.

---
+++ MySQLClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace MySQLClient
+{ 
+    public class Program
+    {
+        static string conStr = "Server=206-P;Database=testDB;Trusted_Connection=True;TrustServerCertificate=true";
+        static void Main()
+        {
+            TestConnection();
+        }
+
+        static void TestConnection()
+        {
+            using (SqlConnection db = new SqlConnection(conStr))
+            {
+                Console.WriteLine(db.State.ToString());
+                db.Open();
+                Console.WriteLine(db.State.ToString());
+                db.Close();
+                Console.WriteLine(db.State.ToString());
+            }
+        }
+    }
+}
