c366650cb370daee8d2fba7c6951811345af7d7f
mbaibatyr <murat_b@mail.ru>
2024-12-20 20:03:27 +0500

    +

---
+++ MySQLClient/Ado.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySQLClient
+{
+    internal class Ado
+    {
+        static string conStr = "Server=204-P;Database=MyDB;Trusted_Connection=True;TrustServerCertificate=true";
+        static void Select()
+        {
+            using (SqlConnection db = new SqlConnection(conStr))
+            {
+                db.Open();
+                DataTable dt = new DataTable();
+                using (SqlCommand cmd = new SqlCommand("pCity", db))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    dt.Load(cmd.ExecuteReader());
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        Console.WriteLine($"{row["id"].ToString()} - {row[1].ToString()} - {row[2].ToString()}- {row[3].ToString()}");
+                    }
+                }
+                db.Close();
+            }
+        }
+    }
+}
