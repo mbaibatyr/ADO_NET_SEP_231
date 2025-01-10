b305442402c4a38e10dfdbe7b0d2ed514708e43c
mbaibatyr <murat_b@mail.ru>
2025-01-10 20:20:53 +0500

    ++

---
+++ MyEFCore/MyContext/Context.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyEFCore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEFCore.MyContext
+{
+    public class Context : DbContext
+    {
+        public DbSet<Country> Country { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {            
+            optionsBuilder.UseSqlServer(Program.config["db"]);
+            //optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\Temp\\db_test.mdf;Integrated Security=True;TrustServerCertificate=true;");
+            //optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
+        }
+    }
+}
---
+++ MyEFCore/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace MyEFCore
+{
+    public class Program
+    {
+        public static IConfigurationRoot config;
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
