5a895585a16d01c36577bbd09db2906363a269c5
mbaibatyr <murat_b@mail.ru>
2025-02-21 19:40:50 +0500

    +

---
+++ MySwagger/Controllers/MyController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySwagger.Model;
+
+namespace MySwagger.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MyController : ControllerBase
+    {
+        [HttpPost, Route("MyPost")]
+        public ActionResult MyPost(Student student)
+        {
+            return Ok("Hello MyPost " + student.Name);
+        }
+
+        [HttpPut, Route("MyPut")]
+        public ActionResult MyPut()
+        {
+            return Ok("Hello MyPut");
+        }
+
+        [HttpGet, Route("MyGet/{id}")]
+        public ActionResult MyGet(string id)
+        {
+            return Ok("Hello MyGet " + id);
+        }
+
+    }
+}
---
+++ MySwagger/Model/Student.cs
@@ -0,0 +1,20 @@
+﻿namespace MySwagger.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Name2 { get; set; }
+        public string Name3 { get; set; }
+        public string Name4 { get; set; }
+        public List<StudentAddress> Address { get; set; }
+    }
+
+    public class StudentAddress
+    {
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string House { get; set; }
+    }
+
+}
