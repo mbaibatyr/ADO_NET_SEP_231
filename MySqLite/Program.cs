c539e049a09ce895f090d8ad5bbfd7415070ac59
mbaibatyr <murat_b@mail.ru>
2025-02-21 19:51:21 +0500

    +

--- MySqLite/Program.cs
+++ MySqLite/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace MySqLite
+﻿using Microsoft.Data.Sqlite;
+
+namespace MySqLite
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MySqlite.GetData();
+        }
+    }
+
+    class MySqlite
+    {
+        public static void GetData()
+        {
+            using (var db = new SqliteConnection(@"Data Source=C:\Users\байбатыровм\Documents\sqlite\MyDB.db;"))
+            {
+                try
+                {
+                    db.Open();
+                    using (var cmd = new SqliteCommand("select * from country", db))
+                    {
+                        var dr = cmd.ExecuteReader();
+                        while (dr.Read())
+                        {
+                            Console.WriteLine($"{dr[0]} - {dr[1]}");
+                        }
+                    }
+                }
+                catch {}
+                finally
+                {
+                    db.Close();
+                }
+                
+            }
         }
     }
 }
